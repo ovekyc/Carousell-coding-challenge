9dde9263fa542fb1b2a107456e085f30bf780193
Youngchan Kim <ovekyc@gmail.com>
2017-05-16 22:02:20 +0900

    fixed lint warning

--- src/server/controllers/topic.js
+++ src/server/controllers/topic.js
@@ -1,15 +1,15 @@
 export default class TopicController {
-  static getTop20(req, res, cb){
+  static getTop20(req, res, cb) {
     res.send('top20');
     cb();
   }
 
-  static getTopic(req, res, cb){
+  static getTopic(req, res, cb) {
     res.send('get t');
     cb();
   }
 
-  static addTopic(req, res, cb){
+  static addTopic(req, res, cb) {
     res.send('post t');
     cb();
   }
