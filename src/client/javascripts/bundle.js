f52f1da4e109408dd0c0fc3b686b3869c59621d7
Youngchan Kim <ovekyc@gmail.com>
2017-05-17 09:55:43 +0900

    Defined boilerplate of view

---
+++ src/client/javascripts/bundle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../../shared/containers/app-container';
+
+ReactDOM.render(
+  <App/>, document.getElementById('app-container')
+);
---
+++ src/shared/containers/app-container.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
+import Home from '../components/home';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default AppContainer;
